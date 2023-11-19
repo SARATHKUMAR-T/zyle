bc7b816c287e5e03f0f56f184b7fac4537255b36
SARATHKUMAR-T <sarathkumartk98@gmail.com>
2023-11-19 11:37:46 +0530

    font changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Inter, Montserrat } from "next/font/google";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import QueryProvider from "@/components/query-provider";
